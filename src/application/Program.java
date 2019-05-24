02d7d96b38f217fe69e7d1739e0c7e10e4fd1b9a
Saulo Bastos <saulo1bas@gmail.com>
2019-05-24 17:46:49 -0300

    findAll SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,18 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Seller;
 
 public class Program {
 	
 	
 	public static void main(String[] args) {
-		Department dep = new Department(1, "RH");
+		SellerDAO sellerDao = DAOFactory.createSellerDao();
 		
-		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,dep);
-		System.out.println(seller);
+		Seller sel = sellerDao.findById(3);
+		
+		System.out.println(sel);
 	}
 	
 }
--- src/model/dao/DAOFactory.java
+++ src/model/dao/DAOFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DAOFactory {
 	
 	public static SellerDAO createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
