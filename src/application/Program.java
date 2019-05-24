e393cc57f90abcb25ffc2ea97861fb1b61bd11a3
Saulo Bastos <saulo1bas@gmail.com>
2019-05-24 17:09:11 -0300

    Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	
 	
 	public static void main(String[] args) {
 		Department dep = new Department(1, "RH");
-		System.out.println(dep.toString());
+		
+		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,dep);
+		System.out.println(seller);
 	}
 	
 }
